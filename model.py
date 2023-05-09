0d0725d98a91a9b3810a22c68d36059aa66a2df4
Hansa-Hettiarachchi <hansahettiarachchi@gmail.com>
2023-05-09 14:17:15 +0530

    test 1

--- model.py
+++ model.py
@@ -61,3 +61,17 @@ class UNET(nn.Module):
             x = self.ups[idx+1](concat_skip)
 
         return self.final_conv(x)
+
+
+def test():
+    x = torch.randn((3, 1, 160, 160))
+    model = UNET(in_channels=1, out_channels=1)
+    preds = model(x)
+
+    print(preds.shape)
+    print(x.shape)
+    assert preds.shape == x.shape
+    
+    
+if __name__ == "__main__":
+    test()
